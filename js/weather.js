dde0f85a1a410853b3cb4e260f7767d012a9a81e
kth919 <kth9193@gmail.com>
2019-05-24 09:40:17 +0900

    실시간 온도 추가

--- js/weather.js
+++ js/weather.js
@@ -1,3 +1,6 @@
+const weather = document.querySelector(".js-weather");
+
+
 const COORDS = 'coords';
 
 function saveCoords(coordsObj) {
@@ -15,6 +18,7 @@ function handleGeoSuccess(position) {
     };
 
     saveCoords(coordsObj);
+    getWeather(latitude, longitute);
 }
 
 function handleGeoError() {
@@ -31,8 +35,22 @@ function loadCoords() {
     if (loadedCoords === null) {
         askForCoords();
     } else {
-
+        const parseCoords = JSON.parse(loadedCoords);
+        console.log(parseCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitute);
     }
+}
+
+function getWeather(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
+        .then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            const temperature = json.main.temp;
+            const place = json.name;
+
+            weather.innerHTML = `${temperature} @ ${place}`;
+        });
 }
 
 function init() {
